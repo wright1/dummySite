56a6dbf174d70098109f730336757739d9e59907
wright1 <prosperwright@yahoo.co.uk>
2019-08-19 20:41:11 +0100

    intial styling

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,25 +1,63 @@
 import React from "react"
 import styled from "styled-components"
 
+const NavDiv = styled.div`
+height: 100vh;
+width: 0;
+position: fixed;
+left: 0;
+top:0;
+background-color: rgba(0, 0, 0, 0.9)
+> a{
+    text-decoration: none;
+    position: absolute;
+    top: 5vh;
+    right: 5vw;
+    font-size: 1.8em;
+}
+`
+
+const NavContent = styled.div`
+position: relative;
+top: 25vh;
+width: 100vw;
+text-align: center;
+> a{
+    padding: 2vw;
+    text-decoration: none;
+    font-size: 1.3em;
+    color: red;
+    display: block;
+
+}
+a:hover{
+    color: green;
+}
+`
+
+
 const Nav = ({ onClick }) => {
+    return(
 
 
-<div id="nav"   className="overlay">
+<NavDiv id="nav" >
 
  {/* Button to close the overlay naviation  */}
-<a href="#" className="closebtn" onClick={onClick()}>&times;</a>
+<a href="#" className="closebtn" onClick={() => onClick()}>&times;</a>
 
 {/* overlay content */}
-<div>
+<NavContent>
     <a href="#">about</a>
     <a href="#">services</a>
     <a href="#">prices</a>
     <a href="#">contact</a> 
-</div>
+</NavContent>
 
 
 
-</div>
+</NavDiv>
 
 
-}
+    )}
+
+export default Nav
